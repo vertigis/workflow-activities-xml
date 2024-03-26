9a14b31f7191641b78e0d89aa81fe7a2c77df2f2
Ryan Cooney <rcooney@users.noreply.github.com>
2024-03-26 16:59:08 -0700

    feat!: Node 20 (#12)
    - Update to Node 20
    - Update to latest workflow SDK

--- src/activities/ConvertToXml.ts
+++ src/activities/ConvertToXml.ts
@@ -1,4 +1,4 @@
-import type { IActivityHandler } from "@geocortex/workflow/runtime";
+import type { IActivityHandler } from "@vertigis/workflow";
 import { XMLBuilder } from "fast-xml-parser";
 
 interface ConvertToXmlInputs {
